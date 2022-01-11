c8e8c871d78446bbac40d34ab12a5602a7946b6d
01tu01 <87894726+01tu01@users.noreply.github.com>
2022-01-11 20:36:23 +0800

    ch9函数式文件

---
+++ ch9 function/ex9_61.m
@@ -0,0 +1,2 @@
+function f=ex6_61(x)
+f=exp(sin(x(1)+2)-0.1*x(2))+cos(x(3));
---
+++ ch9 function/ex9_62.m
@@ -0,0 +1,3 @@
+function [c,ceq]=ex6_62(x)
+c=[x(1)*x(3)-x(2)-100,exp(x(1))-x(2)-100];
+ceq=[log(x(1)+1)-sin(x(2))+x(3)-10];
